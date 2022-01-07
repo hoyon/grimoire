39ac19d7a0fd4215d7cc1b622648b6f0c40840ac
Ho-Yon Mak <hoyon.mak@gmail.com>
2022-01-07 16:17:43 +0000

    add optional flag

--- lib/grimoire_web/views/spell_view.ex
+++ lib/grimoire_web/views/spell_view.ex
@@ -1,3 +1,9 @@
 defmodule GrimoireWeb.SpellView do
   use GrimoireWeb, :view
+
+  def required?(%{opts: opts}) do
+    Keyword.get(opts, :optional, false) == false
+  end
+
+  def required?(_), do: true
 end
