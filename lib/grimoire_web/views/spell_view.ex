164105f85a512105f22fdc18a4ef01f22e787b76
Ho-Yon Mak <ho-yon.mak@multiverse.io>
2022-05-10 09:51:34 +0100

    delete old controllers

--- lib/grimoire_web/views/spell_view.ex
+++
@@ -1,9 +0,0 @@
-defmodule GrimoireWeb.SpellView do
-  use GrimoireWeb, :view
-
-  def required?(%{opts: opts}) do
-    Keyword.get(opts, :optional, false) == false
-  end
-
-  def required?(_), do: true
-end
